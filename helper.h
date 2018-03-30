b1346412a753ad44fe06d6d6769b4429e4c66ed0
Giuseppe Corbelli <corbelligiuseppe@mesdan.it>
2018-03-30 09:58:13 +0200

    Added helper.h to overcome a code generation issue

---
+++ helper.h
@@ -0,0 +1,7 @@
+#if !defined(__QCUSTOMPLOT_SIP_HELPER)
+#define __QCUSTOMPLOT_SIP_HELPER
+
+typedef QCPAbstractPlottable1D<QCPGraphData> QCPAbstractPlottable1D_QCPGraphData;
+typedef QCPAbstractPlottable1D<QCPBarsData> QCPAbstractPlottable1D_QCPBarsData;
+
+#endif  // __QCUSTOMPLOT_SIP_HELPER
