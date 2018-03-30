7afc157998eeb0f57ee50496c07440d0df0227d2
Giuseppe Corbelli <corbelligiuseppe@mesdan.it>
2018-03-30 14:39:49 +0200

    Ported SimpleDemo (example nr 1)

--- examples/plots/mainwindow.py
+++ examples/plots/mainwindow.py
@@ -9,11 +9,17 @@
 # QCustomPlot author: Emanuel Eichhammer
 # QCustomPlot Website/Contact: http://www.qcustomplot.com
 
+import math
+
+from PyQt5 import Qt
 from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QPen, QBrush, QColor
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.uic import loadUi
 
 import QCustomPlot
+
+from QCustomPlot import QCP
 
 class MainWindow(QMainWindow):
     def __init__(self, argv, parent=None):
@@ -22,7 +28,7 @@ class MainWindow(QMainWindow):
 
         self._available_demos = {
             0: self.setupQuadraticDemo,
-#             1: self.setupSimpleDemo,
+            1: self.setupSimpleDemo,
 #             2: self.setupSincScatterDemo,
 #             3: self.setupScatterStyleDemo,
 #             4: self.setupScatterPixmapDemo,
@@ -80,7 +86,44 @@ class MainWindow(QMainWindow):
         self.customPlot.xAxis.setRange(-1, 1)
         self.customPlot.yAxis.setRange(0, 1)
 
-#   void setupSimpleDemo(QCustomPlot *customPlot);
+    def setupSimpleDemo(self):
+        self.demoName = "Simple Demo"
+
+        # add two new graphs and set their look:
+        self.customPlot.addGraph()
+        self.customPlot.graph(0).setPen(QPen(QColor("blue")))  # line color blue for first graph
+        self.customPlot.graph(0).setBrush(QBrush(QColor(0, 0, 255, 20)))  # first graph will be filled with translucent blue
+        self.customPlot.addGraph();
+        self.customPlot.graph(1).setPen(QPen(QColor("red")))  # line color red for second graph
+        # generate some points of data (y0 for first, y1 for second graph):
+        x = [0.0] * 251
+        y0 = [0.0] * 251
+        y1 = [0.0] * 251
+        for i in range(0, 251):
+             x[i] = float(i)
+             y0[i] = pow(math.e, -i/150.0) * math.cos(i/10.0)  # exponentially decaying cosine
+             y1[i] = pow(math.e, -i/150.0)  # exponential envelope
+
+        # configure right and top axis to show ticks but no labels:
+        # (see QCPAxisRect::setupFullAxesBox for a quicker method to do this)
+        self.customPlot.xAxis2.setVisible(True)
+        self.customPlot.xAxis2.setTickLabels(False)
+        self.customPlot.yAxis2.setVisible(True)
+        self.customPlot.yAxis2.setTickLabels(False)
+        # make left and bottom axes always transfer their ranges to right and top axes:
+        self.customPlot.xAxis.rangeChanged.connect(self.customPlot.xAxis2.setRange)
+        self.customPlot.yAxis.rangeChanged.connect(self.customPlot.yAxis2.setRange)
+        # pass data points to graphs:
+        self.customPlot.graph(0).setData(x, y0)
+        self.customPlot.graph(1).setData(x, y1)
+        # let the ranges scale themselves so graph 0 fits perfectly in the visible area:
+        self.customPlot.graph(0).rescaleAxes()
+        # same thing for graph 1, but only enlarge ranges (in case graph 1 is smaller than graph 0):
+        self.customPlot.graph(1).rescaleAxes(True)
+        # Note: we could have also just called customPlot->rescaleAxes(); instead
+        # Allow user to drag axis ranges with mouse, zoom with mouse wheel and select graphs by clicking:
+        # TODO: figure out how to skip the explicit intermediate QCP.Interactions
+        self.customPlot.setInteractions(QCustomPlot.QCP.Interactions(QCP.iRangeDrag | QCP.iRangeZoom | QCP.iSelectPlottables))
 #   void setupSincScatterDemo(QCustomPlot *customPlot);
 #   void setupScatterStyleDemo(QCustomPlot *customPlot);
 #   void setupLineStyleDemo(QCustomPlot *customPlot);
@@ -101,17 +144,3 @@ class MainWindow(QMainWindow):
 #   void setupFinancialDemo(QCustomPlot *customPlot);
 #
 #   void setupPlayground(QCustomPlot *customPlot);
-
-# private slots:
-#   void realtimeDataSlot();
-#   void bracketDataSlot();
-#   void screenShot();
-#   void allScreenShots();
-#
-# private:
-#   Ui::MainWindow *ui;
-#   QString demoName;
-#   QTimer dataTimer;
-#   QCPItemTracer *itemDemoPhaseTracer;
-#   int currentDemoIndex;
-# };
