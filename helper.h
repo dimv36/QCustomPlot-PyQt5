ce411194bec46135c5b6a23aaa784a73cce4bddd
Giuseppe Corbelli <corbelligiuseppe@mesdan.it>
2018-10-03 10:29:07 +0200

    Added a typedef for QCPAbstractPlottable1D<QCPCurveData>

--- helper.h
+++ helper.h
@@ -3,5 +3,6 @@
 
 typedef QCPAbstractPlottable1D<QCPGraphData> QCPAbstractPlottable1D_QCPGraphData;
 typedef QCPAbstractPlottable1D<QCPBarsData> QCPAbstractPlottable1D_QCPBarsData;
+typedef QCPAbstractPlottable1D<QCPCurveData> QCPAbstractPlottable1D_QCPCurveData;
 
 #endif  // __QCUSTOMPLOT_SIP_HELPER
